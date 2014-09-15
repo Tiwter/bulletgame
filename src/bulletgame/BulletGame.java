3859c60897b4cdcfccb27fcaf8101c372d70c96b
samsung <samsung@samsung-PC>
2014-09-15 10:23:39 +0700

    2

---
+++ src/bulletgame/BulletGame.java
@@ -0,0 +1,40 @@
+package bulletgame;
+
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+
+public class BulletGame extends BasicGame{
+
+	public BulletGame(String title) {
+		super(title);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void render(GameContainer arg0, Graphics arg1) throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void init(GameContainer arg0) throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(GameContainer arg0, int arg1) throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+	public interface Entity{
+		void render(Graphics g);
+		void update(int delta);
+	}
+	public static void main(String[] args){
+		
+	}
+
+}
