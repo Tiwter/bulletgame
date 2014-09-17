75b5f9493dbc25427813db952a629d8371375a71
samsung <samsung@samsung-PC>
2014-09-17 20:55:27 +0700

    Bullet fall

--- src/bulletgame/Bullet.java
+++ src/bulletgame/Bullet.java
@@ -1,21 +1,23 @@
 package bulletgame;
 
 import org.newdawn.slick.Graphics;
-
-import bulletgame.BulletGame.Entity;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
 
 public class Bullet implements Entity{
 	private static final float BULLET_SIZE = 5;
 	private float x;
 	private float y;
+	private Image image;
 	
-	public Bullet(float x, float y){
+	public Bullet(float x, float y) throws SlickException{
 		this.setXY(x,y);
+		//image = new Image("res/bullet.png");
 	}
 	
 	@Override
 	public void render(Graphics g){
-		g.fillOval(getX(), getY(), BULLET_SIZE, BULLET_SIZE);;
+		g.fillOval(getX(), getY(), BULLET_SIZE, BULLET_SIZE);
 	}
 	
 	@Override
--- src/bulletgame/BulletGame.java
+++ src/bulletgame/BulletGame.java
@@ -1,5 +1,8 @@
 package bulletgame;
 
+import java.util.LinkedList;
+
+import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.BasicGame;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
@@ -7,34 +10,52 @@ import org.newdawn.slick.SlickException;
 
 public class BulletGame extends BasicGame{
 
+	public static final int GAME_WIDTH = 640;
+	public static final int GAME_HEIGHT = 480;
+	private LinkedList<Entity> entities;
+	private Graphics g;
+	private int delta;
+	
 	public BulletGame(String title) {
 		super(title);
+		entities = new LinkedList<Entity>();
 		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public void render(GameContainer arg0, Graphics arg1) throws SlickException {
+		for(Entity entity : entities){
+			entity.render(arg1);
+		}
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void init(GameContainer arg0) throws SlickException {
+		entities.add(new Bullet(200,0));
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void update(GameContainer arg0, int arg1) throws SlickException {
+		for(Entity entity : entities){
+			entity.update(arg1);
+		}
 		// TODO Auto-generated method stub
 		
 	}
-	public interface Entity{
-		void render(Graphics g);
-		void update(int delta);
-	}
 	public static void main(String[] args){
-		
+		 try {
+			 BulletGame game = new BulletGame("Bullet Game");
+		     AppGameContainer appgc = new AppGameContainer(game);
+		     appgc.setDisplayMode(GAME_WIDTH, GAME_HEIGHT, false);
+		     appgc.setTargetFrameRate(60);
+		     appgc.start();
+		 } catch (SlickException e) {
+		     e.printStackTrace();
+		   }
 	}
 
 }
